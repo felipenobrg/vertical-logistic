0731e5b7c41b4ac07416b4cf0c73a6a1b5da6ea5
felipenobrg <felipenobrega2012@gmail.com>
2025-05-13 11:38:10 -0300

    first commit

---
+++ src/main/java/com/challenge/verticallogistics/VerticallogisticsApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.verticallogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerticallogisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerticallogisticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/challenge/verticallogistics/VerticallogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.verticallogistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerticallogisticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
