232d9b9102d0954383930eccd99e2816e5b4ce1b
felipenobrg <felipenobrega2012@gmail.com>
2025-05-14 08:54:34 -0300

    fix: add new lombok anotations

--- src/main/java/com/challenge/verticallogistics/dto/response/OrderResponse.java
+++ src/main/java/com/challenge/verticallogistics/dto/response/OrderResponse.java
@@ -1,11 +1,16 @@
 package com.challenge.verticallogistics.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderResponse {
     private Long order_id;
     private String total;
