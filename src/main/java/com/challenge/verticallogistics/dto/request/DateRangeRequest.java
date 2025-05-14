d63818f7ec02f630ef01c3968476d68b877997aa
felipenobrg <felipenobrega2012@gmail.com>
2025-05-14 08:54:12 -0300

    fix: change DateRangeRequest class

--- src/main/java/com/challenge/verticallogistics/dto/request/DateRangeRequest.java
+++ src/main/java/com/challenge/verticallogistics/dto/request/DateRangeRequest.java
@@ -3,17 +3,10 @@ package com.challenge.verticallogistics.dto.request;
 
 import lombok.Data;
 import java.time.LocalDate;
-import org.springframework.format.annotation.DateTimeFormat;
-import jakarta.validation.constraints.Pattern;
 
 @Data
 public class DateRangeRequest {
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-    private LocalDate startDate;
-
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-    private LocalDate endDate;
-
-    @Pattern(regexp = "^\\d+$", message = "Order ID deve ser numerico")
     private String orderId;
+    private LocalDate startDate;
+    private LocalDate endDate;
 }
